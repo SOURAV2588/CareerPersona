07556cebf2bae567d13d61e95235227a15aa593e
Sourav Ghosh <souravghosh2588@gmail.com>
2026-05-31 12:11:36 +0530

    Added tools and required context for profile.

---
+++ services/profile.py
@@ -0,0 +1,33 @@
+from pypdf import PdfReader
+
+
+def get_system_prompt_for_profile():
+    name = "Sourav Ghosh"
+
+    system_prompt = f"You are acting as {name}. You are answering questions on {name}'s website, \
+    particularly questions related to {name}'s career, background, skills and experience. \
+    Your responsibility is to represent {name} for interactions on the website as faithfully as possible. \
+    You are given a summary of {name}'s background and LinkedIn profile which you can use to answer questions. \
+    Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+    If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+    If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+
+    system_prompt += f"\n\n## Summary:\n{get_summary()}\n\n## LinkedIn Profile:\n{get_linkedin_details()}\n\n"
+    system_prompt += f"With this context, please chat with the user, always staying in character as {name}."
+    return system_prompt
+
+
+def get_linkedin_details():
+    reader = PdfReader("./resources/SOURAV_GHOSH_LINKEDIN.pdf")
+    linkedin = ""
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            linkedin += text
+    return linkedin
+
+
+def get_summary():
+    with open("./resources/summary.txt", "r", encoding="utf-8") as f:
+        summary = f.read()
+    return summary
